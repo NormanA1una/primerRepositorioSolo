34f4c1f8db08cc580622433f72842338f0a99c4e
NormanA1una <baciloferoz@gmail.com>
2023-03-13 16:50:48 -0600

    Cosa de Prettier

--- JS/tema_oscuro.js
+++ JS/tema_oscuro.js
@@ -1,27 +1,26 @@
 const d = document;
 
 export default function darkTheme(btn, classDark, classGray) {
-    const $themeBtn = d.querySelector(btn),
-        $selectors = d.querySelectorAll("[data-dark]"),
-        $stageDarkMode = d.querySelector(".stage");
-    
-        console.log($selectors);
+  const $themeBtn = d.querySelector(btn),
+    $selectors = d.querySelectorAll("[data-dark]"),
+    $stageDarkMode = d.querySelector(".stage");
 
-        let moon= "🌙",
-            sun= "☀️";
+  console.log($selectors);
 
-    d.addEventListener("click", (e) => {
-        if (e.target.matches(btn)){
-            if ($themeBtn.textContent === moon) {
-                $selectors.forEach(el => el.classList.add(classDark));
-                $stageDarkMode.classList.add(classGray);
-                $themeBtn.textContent = sun;
-            } else {
-                $selectors.forEach(el => el.classList.remove(classDark));
-                $stageDarkMode.classList.remove(classGray);
-                $themeBtn.textContent = moon;
-            }
-            
-        }
-    })
+  let moon = "🌙",
+    sun = "☀️";
+
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(btn)) {
+      if ($themeBtn.textContent === moon) {
+        $selectors.forEach((el) => el.classList.add(classDark));
+        $stageDarkMode.classList.add(classGray);
+        $themeBtn.textContent = sun;
+      } else {
+        $selectors.forEach((el) => el.classList.remove(classDark));
+        $stageDarkMode.classList.remove(classGray);
+        $themeBtn.textContent = moon;
+      }
+    }
+  });
 }
